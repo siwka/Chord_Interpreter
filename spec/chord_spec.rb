bb50b592e57601822826ea8ad1d10a1f2445452d
siwka <ms.ksiwek@gmail.com>
2014-02-24 03:24:27 -0600

    basic major chords

--- spec/chord_spec.rb
+++ spec/chord_spec.rb
@@ -3,11 +3,29 @@ require_relative 'spec_helper'
 describe Chord do
 	chord = Chord.new
 
-	it "returns C E G from input 'C'" do
-		chord.chord_interpretator('C').should eq("C E G")
-	end
 
-	it "returns D F A from input 'D'" do
-		chord.chord_interpretator('D').should eq("D Fis A")
-	end
+	it { chord.chord_interpretator('C').should eq('C E G') }
+	it { chord.chord_interpretator('D').should eq('D F# A') }
+	it { chord.chord_interpretator('E').should eq('E G# B') }
+	it { chord.chord_interpretator('F').should eq('F A C') }
+	it { chord.chord_interpretator('G').should eq('G B D') }
+	it { chord.chord_interpretator('A').should eq('A C# E') }	
+	it { chord.chord_interpretator('B').should eq('B D# F#') }
+
+	it { chord.chord_interpretator('C#').should eq('C# F G#') }
+	it { chord.chord_interpretator('D#').should eq('D# G A#') }
+	it { chord.chord_interpretator('E#').should eq('E# A C') }	
+	it { chord.chord_interpretator('F#').should eq('F# A# C#') }
+	it { chord.chord_interpretator('G#').should eq('G# C D#') }
+	it { chord.chord_interpretator('A#').should eq('A# D F') }
+	it { chord.chord_interpretator('B#').should eq('B# E G') }
+
+	it { chord.chord_interpretator('Cb').should eq('Cb Eb Gb') }
+	it { chord.chord_interpretator('Db').should eq('Db F Ab') }
+	it { chord.chord_interpretator('Eb').should eq('Eb G Bb') }
+	it { chord.chord_interpretator('Fb').should eq('Fb Ab B') }
+	it { chord.chord_interpretator('Gb').should eq('Gb Bb Db') }
+	it { chord.chord_interpretator('Ab').should eq('Ab C Eb') }	
+	it { chord.chord_interpretator('Bb').should eq('Bb D F') }
+		
 end
