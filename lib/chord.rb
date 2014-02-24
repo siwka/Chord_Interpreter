8cc22a0959b140dd78a3652c9505756d7014aa7c
siwka <ms.ksiwek@gmail.com>
2014-02-24 03:30:16 -0600

    basic major chords in chord.rb

--- lib/chord.rb
+++ lib/chord.rb
@@ -1,15 +1,27 @@
 class Chord
 
 	def chord_interpretator(chord_notation)
-		tonacja = %w(C Cis D Dis E F Fis G Gis A Ais B)
+		chromatic_scale_sharp = %w(C C# D D# E F F# G G# A A# B)
+		chromatic_scale_flat = %w(C Db D Eb E F Gb G Ab A Bb B)
 
-		if chord_notation == 'C'
-			return tonacja[0] + ' ' +  tonacja[4] + ' ' + tonacja[7]
-		elsif chord_notation == "D"
-			return tonacja[2] + ' ' +  tonacja[6] + ' ' + tonacja[9]
-			
-		end
+		interval = chromatic_scale_flat.index(chord_notation[0])
 
+		if chord_notation[1] == 'b'
+			interval -= 1
+			return chord_notation[0..1] + ' '	+  chromatic_scale_flat[transposition(4, interval)] + ' ' 	+ chromatic_scale_flat[transposition(7, interval)]
+		elsif chord_notation[1] == '#'
+			interval +=1
+			return chord_notation[0..1] + ' '	+  chromatic_scale_sharp[transposition(4, interval)] + ' ' 	+ chromatic_scale_sharp[transposition(7, interval)]
+		end				
+		chord_notation[0] + ' '	+  chromatic_scale_sharp[transposition(4, interval)] + ' ' 	+ chromatic_scale_sharp[transposition(7, interval)]
+	end
+
+
+	def transposition(note, interval)
+		(note + interval) % 12
+	end
+
+	def fun
 	end
 
 end
