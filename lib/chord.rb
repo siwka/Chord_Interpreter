2e8b1ac538a1111d3ba7a3a6ae773a511693403c
siwka <ms.ksiwek@gmail.com>
2014-02-25 02:48:58 -0600

    change nr of arguments for method

--- lib/chord.rb
+++ lib/chord.rb
@@ -1,27 +1,62 @@
 class Chord
+	#attr_accessor :chord_notation
+	
+	#def initialize()
 
-	def chord_interpretator(chord_notation)
+	#end
+
+	def chord_interpretator(chord_root, chord_quality, chord_interval, alerted_fifth, added_tone_chords)
 		chromatic_scale_sharp = %w(C C# D D# E F F# G G# A A# B)
 		chromatic_scale_flat = %w(C Db D Eb E F Gb G Ab A Bb B)
-		maj3 = 4
-		perf5 = 7
+		
+		# Third
+		min3 = 3 # Eb
+		maj3 = 4  # E no_symbol, M, maj, or major
 
-		interval = chromatic_scale_flat.index(chord_notation[0])
+		# Fifth
+		dim5 = 6 # Gb
+		perf5 = 7 # G no_symbol, M, maj, or major
+		aug5 = 8 # G#
 
-		chord_root = chord_notation[0]
-		chord_third = maj3
-		chord_fifth = perf5
+		# Added
+		maj6 = 9 # A =min6  to moze zapisac min6 & maj6 & M6 & m6 ma pozycje 9
+		dim7 = 0 # Bbb
+		min7 = 10 # Bb
+		maj7 = 11 # B
 
-		if chord_notation[1] == 'b'
+		#chord_root = chord_notation[0]
+		interval = chromatic_scale_flat.index(chord_root[0])
+
+		if chord_root[1] == 'b'
+			flat = true
 			interval -= 1
-			chord_root << chord_notation[1]
-			return chord_root + ' '	+  chromatic_scale_flat[transposition(chord_third, interval)] + ' ' 	+ chromatic_scale_flat[transposition(chord_fifth, interval)]
-		elsif chord_notation[1] == '#'
+		elsif chord_root[1] == '#'
+			sharp = true
 			interval +=1
-			chord_root << chord_notation[1]
+		end
+			
+		case 
+		when chord_quality == 'maj' 
+			chord_third = maj3
+			chord_fifth = perf5
+		when chord_quality == 'min'
+			chord_third = min3
+			chord_fifth = perf5
+		end
+
+
+		if flat
+			chord_third = chromatic_scale_flat[transposition(chord_third, interval)]
+			chord_fifth = chromatic_scale_flat[transposition(chord_fifth, interval)]
+		elsif sharp
+			chord_third = chromatic_scale_sharp[transposition(chord_third, interval)]
+			chord_fifth = chromatic_scale_sharp[transposition(chord_fifth, interval)]
+		else
+			chord_third = chromatic_scale_sharp[transposition(chord_third, interval)]
+			chord_fifth = chromatic_scale_sharp[transposition(chord_fifth, interval)]
 		end
 		
-		chord_root + ' '	+  chromatic_scale_sharp[transposition(chord_third, interval)] + ' ' 	+ chromatic_scale_sharp[transposition(chord_fifth, interval)]
+		chord_root + ' '	+  chord_third + ' '	+  chord_fifth
 	end
 
 
--- spec/chord_spec.rb
+++ spec/chord_spec.rb
@@ -4,28 +4,33 @@ describe Chord do
 	chord = Chord.new
 
 
-	it { chord.chord_interpretator('C').should eq('C E G') }
-	it { chord.chord_interpretator('D').should eq('D F# A') }
-	it { chord.chord_interpretator('E').should eq('E G# B') }
-	it { chord.chord_interpretator('F').should eq('F A C') }
-	it { chord.chord_interpretator('G').should eq('G B D') }
-	it { chord.chord_interpretator('A').should eq('A C# E') }	
-	it { chord.chord_interpretator('B').should eq('B D# F#') }
+	it { chord.chord_interpretator('C','maj',7,1,1).should eq('C E G') }
+	it { chord.chord_interpretator('D','maj',7,1,1).should eq('D F# A') }
+	it { chord.chord_interpretator('E','maj',7,1,1).should eq('E G# B') }
+	it { chord.chord_interpretator('F','maj',7,1,1).should eq('F A C') }
+	it { chord.chord_interpretator('G','maj',7,1,1).should eq('G B D') }
+	it { chord.chord_interpretator('A','maj',7,1,1).should eq('A C# E') }	
+	it { chord.chord_interpretator('B','maj',7,1,1).should eq('B D# F#') }
 
-	it { chord.chord_interpretator('C#').should eq('C# F G#') }
-	it { chord.chord_interpretator('D#').should eq('D# G A#') }
-	it { chord.chord_interpretator('E#').should eq('E# A C') }	
-	it { chord.chord_interpretator('F#').should eq('F# A# C#') }
-	it { chord.chord_interpretator('G#').should eq('G# C D#') }
-	it { chord.chord_interpretator('A#').should eq('A# D F') }
-	it { chord.chord_interpretator('B#').should eq('B# E G') }
+	it { chord.chord_interpretator('C#','maj',7,1,1).should eq('C# F G#') }
+	it { chord.chord_interpretator('D#','maj',7,1,1).should eq('D# G A#') }
+	it { chord.chord_interpretator('E#','maj',7,1,1).should eq('E# A C') }	
+	it { chord.chord_interpretator('F#','maj',7,1,1).should eq('F# A# C#') }
+	it { chord.chord_interpretator('G#','maj',7,1,1).should eq('G# C D#') }
+	it { chord.chord_interpretator('A#','maj',7,1,1).should eq('A# D F') }
+	it { chord.chord_interpretator('B#','maj',7,1,1).should eq('B# E G') }
 
-	it { chord.chord_interpretator('Cb').should eq('Cb Eb Gb') }
-	it { chord.chord_interpretator('Db').should eq('Db F Ab') }
-	it { chord.chord_interpretator('Eb').should eq('Eb G Bb') }
-	it { chord.chord_interpretator('Fb').should eq('Fb Ab B') }
-	it { chord.chord_interpretator('Gb').should eq('Gb Bb Db') }
-	it { chord.chord_interpretator('Ab').should eq('Ab C Eb') }	
-	it { chord.chord_interpretator('Bb').should eq('Bb D F') }
+	it { chord.chord_interpretator('Cb','maj',7,1,1).should eq('Cb Eb Gb') }
+	it { chord.chord_interpretator('Db','maj',7,1,1).should eq('Db F Ab') }
+	it { chord.chord_interpretator('Eb','maj',7,1,1).should eq('Eb G Bb') }
+	it { chord.chord_interpretator('Fb','maj',7,1,1).should eq('Fb Ab B') }
+	it { chord.chord_interpretator('Gb','maj',7,1,1).should eq('Gb Bb Db') }
+	it { chord.chord_interpretator('Ab','maj',7,1,1).should eq('Ab C Eb') }	
+	it { chord.chord_interpretator('Bb','maj',7,1,1).should eq('Bb D F') }
+
+
+	it { chord.chord_interpretator('C', 'min',7,1,1).should eq('C Eb G') }
+	it { chord.chord_interpretator('D', 'min',7,1,1).should eq('D F A') }
+	it { chord.chord_interpretator('E', 'min',7,1,1).should eq('E G B') }	
 		
 end
