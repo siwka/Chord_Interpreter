ee23076131c7d79cdd9f2d0d1c1f80ba2f4cfcfc
siwka <ms.ksiwek@gmail.com>
2014-02-25 19:08:05 -0600

    added diminished chords

--- lib/chord.rb
+++ lib/chord.rb
@@ -5,7 +5,7 @@ class Chord
 
 	#end
 
-	def chord_interpretator(chord_root, chord_quality, chord_interval, alerted_fifth, added_tone_chords)
+	def chord_interpretator(chord_root, chord_quality, interval_quality, alerted_fifth, added_tone_chords)
 		chromatic_scale_sharp = %w(C C# D D# E F F# G G# A A# B)
 		chromatic_scale_flat = %w(C Db D Eb E F Gb G Ab A Bb B)
 		chromatic_scale = []
@@ -32,7 +32,8 @@ class Chord
 		end
 
 		interval = chromatic_scale.index(chord_root)
-		interval = chromatic_scale.index('F') if chord_root == 'E#'
+		interval = chromatic_scale.index('F') if chord_root == 'E#' && chord_quality == 'min'
+		interval = chromatic_scale.index('A#') if chord_root == 'Bb' && chord_quality == 'aug'
 			
 		case chord_quality
 		when 'maj'
@@ -41,6 +42,12 @@ class Chord
 		when 'min'
 			chord_third = min3
 			chord_fifth = perf5
+		when 'aug'
+			chord_third = maj3
+			chord_fifth = aug5
+		when 'dim'
+			chord_third = min3
+			chord_fifth = dim5
 		end
 
 		chord_third = chromatic_scale[transposition(chord_third, interval)]
@@ -51,11 +58,17 @@ class Chord
 
 
 	def is_flat(chord_root, chord_quality)
-		(chord_quality == 'maj' && ( %w(F Bb Eb Ab Db Gb Cb Fb).index(chord_root))) || (chord_quality == 'min' && ( %w(D G C F Bb Eb Ab Db Gb).index(chord_root)))
+		%w(maj aug).index(chord_quality) && %w(Eb Ab Db Gb Cb Fb).index(chord_root) \
+				|| chord_quality == 'maj' && %w(F Bb).index(chord_root) \
+				|| %w(min dim).index(chord_quality) && %w(D G C F Bb Eb Ab Db Gb).index(chord_root) \
+				|| chord_quality == 'dim' && %w(A E).index(chord_root)
 	end
 
 	def is_sharp(chord_root, chord_quality)
-		(chord_quality == 'maj' && ( %w(C G D A E B F# C# G# D# A#).index(chord_root))) || (chord_quality == 'min' && ( %w(A E B F# C# G# D# A# E#).index(chord_root)))
+		%w(maj aug).index(chord_quality) &&  %w(C G D A E B F# C# G# D# A#).index(chord_root) \
+				|| chord_quality == 'aug' && %w(F Bb).index(chord_root) \
+				|| %w(min dim).index(chord_quality) &&  %w(A B F# C# G# D# A# E#).index(chord_root) \
+				|| chord_quality == 'min' && %w(A E).index(chord_root)
 	end
 
 
--- spec/chord_spec.rb
+++ spec/chord_spec.rb
@@ -51,4 +51,47 @@ describe Chord do
 	#it { chord.chord_interpretator('B#','min',7,1,1).should eq('B# D# G') } they do not exist -> ERROR CONTROL in TOKENIZATION
 	#it { chord.chord_interpretator('Cb','min',7,1,1).should eq('Cb E Gb') }
 	#it { chord.chord_interpretator('Fb','min',7,1,1).should eq('Fb A B') }
+
+	# augumented chord
+	it { chord.chord_interpretator('C','aug',7,1,1).should eq('C E G#') }
+	it { chord.chord_interpretator('C#','aug',7,1,1).should eq('C# F A') }
+	it { chord.chord_interpretator('Db','aug',7,1,1).should eq('Db F A') }
+	it { chord.chord_interpretator('D','aug',7,1,1).should eq('D F# A#') }
+	it { chord.chord_interpretator('D#','aug',7,1,1).should eq('D# G B') }
+	it { chord.chord_interpretator('Eb','aug',7,1,1).should eq('Eb G B') }	
+	it { chord.chord_interpretator('E','aug',7,1,1).should eq('E G# C') }
+	it { chord.chord_interpretator('F','aug',7,1,1).should eq('F A C#') }
+	it { chord.chord_interpretator('F#','aug',7,1,1).should eq('F# A# D') }
+	it { chord.chord_interpretator('Gb','aug',7,1,1).should eq('Gb Bb D') }
+	it { chord.chord_interpretator('G','aug',7,1,1).should eq('G B D#') }
+	it { chord.chord_interpretator('G#','aug',7,1,1).should eq('G# C E') }
+	it { chord.chord_interpretator('Ab','aug',7,1,1).should eq('Ab C E') }	
+	it { chord.chord_interpretator('A','aug',7,1,1).should eq('A C# F') }	
+	it { chord.chord_interpretator('A#','aug',7,1,1).should eq('A# D F#') }
+	it { chord.chord_interpretator('Bb','aug',7,1,1).should eq('Bb D F#') }
+	it { chord.chord_interpretator('B','aug',7,1,1).should eq('B D# G') }
+
+	# dimished
+	it { chord.chord_interpretator('C', 'dim',7,1,1).should eq('C Eb Gb') }
+	it { chord.chord_interpretator('C#','dim',7,1,1).should eq('C# E G') }
+	it { chord.chord_interpretator('Db','dim',7,1,1).should eq('Db E G') }
+	#it { chord.chord_interpretator('Db','min',7,1,1).should eq('Db Fb Ab') } # both Db are correct - can be considered later
+	it { chord.chord_interpretator('D', 'dim',7,1,1).should eq('D F Ab') }
+	it { chord.chord_interpretator('D#','dim',7,1,1).should eq('D# F# A') }
+	it { chord.chord_interpretator('Eb','dim',7,1,1).should eq('Eb Gb A') }		
+	it { chord.chord_interpretator('E', 'dim',7,1,1).should eq('E G Bb') }
+	#it { chord.chord_interpretator('E#', 'dim',7,1,1).should eq('E# G# C') } # no dim chord -> TOKENIZATION
+	it { chord.chord_interpretator('F','dim',7,1,1).should eq('F Ab B') }
+	it { chord.chord_interpretator('F#','dim',7,1,1).should eq('F# A C') }
+	it { chord.chord_interpretator('Gb','dim',7,1,1).should eq('Gb A C') }
+	#it { chord.chord_interpretator('Gb','dim',7,1,1).should eq('Gb Bbb Db') } # both Gb are correct - can be considered later
+	it { chord.chord_interpretator('G','dim',7,1,1).should eq('G Bb Db') }
+	it { chord.chord_interpretator('G#','dim',7,1,1).should eq('G# B D') }
+	it { chord.chord_interpretator('Ab','dim',7,1,1).should eq('Ab B D') }
+	#it { chord.chord_interpretator('Ab','dim',7,1,1).should eq('Ab Cb Eb') } # both Ab are correct - can be considered later
+	it { chord.chord_interpretator('A','dim',7,1,1).should eq('A C Eb') }	
+	it { chord.chord_interpretator('A#','dim',7,1,1).should eq('A# C# E') }
+	it { chord.chord_interpretator('Bb','dim',7,1,1).should eq('Bb Db E') }
+	it { chord.chord_interpretator('B','dim',7,1,1).should eq('B D F') }		
+
 end
