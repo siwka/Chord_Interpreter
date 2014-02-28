57ebd641772f397e89e1d8c0c86350ff4fa51046
siwka <ms.ksiwek@gmail.com>
2014-02-28 13:37:01 -0600

    refactore chord.rb

--- lib/chord.rb
+++ lib/chord.rb
@@ -9,7 +9,7 @@ class Chord
 		PERF5 = 7	# no_symbol, M, maj, or major
 		AUG5 = 8
 		# Added
-		MAJ6 = 9		# min6  to moze zapisac min6 & maj6 & M6 & m6 ma pozycje 9
+		MAJ6 = 9		# min6  can be written as min6 & maj6 & M6 & m6 ma pozycje 9
 		DIM7 = 9
 		MIN7 = 10	
 		MAJ7 = 11	
@@ -22,9 +22,9 @@ class Chord
 		chord_third = chromatic_scale[transpose(chord_third(chord_quality), interval)]
 		chord_fifth = chromatic_scale[transpose(chord_fifth(chord_quality), interval)]
 
-		if interval_number == 0
-			chord_notes_list1(chord_root, chord_third, chord_fifth)
-		else
+		notes_list = "#{chord_root} #{chord_third} #{chord_fifth}"
+
+		if interval_number != 0 #
 			if interval_number == 7 || interval_number == 'maj7'
 				if chord_quality == 'aug' || chord_quality == 'min'
 					chromatic_scale = determine_scale(chord_root, 'dom')
@@ -32,14 +32,16 @@ class Chord
 					chromatic_scale = CHROMATIC_SCALE_FLAT
 				end
 			end	
-			# interval_number == 6, 7, 'maj7' :
+			# so far works for: interval_number == 6, 7, 'maj7' 
 			interval_added = chromatic_scale[transpose(interval_number(interval_number, chord_quality), interval)]
-			chord_notes_list2(chord_root, chord_third, chord_fifth, interval_added)
+			notes_list << " #{interval_added}"
 		end
+
+  	notes_list
 	end
 
 
-	def is_flat(chord_root, chord_quality)
+	def flat?(chord_root, chord_quality)
 		%w(maj aug dom).index(chord_quality) && %w(Eb Ab Db Gb Cb Fb).index(chord_root) \
 				|| %w(maj dom).index(chord_quality)  && %w(F Bb).index(chord_root) \
 				|| %w(min dim).index(chord_quality) && %w(D G C F Bb Eb Ab Db Gb).index(chord_root) \
@@ -47,7 +49,7 @@ class Chord
 				|| chord_quality == 'dom' && chord_root == 'C'
  	end
 
-	def is_sharp(chord_root, chord_quality)
+	def sharp?(chord_root, chord_quality)
 		%w(maj aug dom).index(chord_quality) &&  %w(C G D A E B F# C# G# D# A#).index(chord_root) \
 				|| chord_quality == 'aug' && %w(F Bb).index(chord_root) \
 				|| %w(min dim).index(chord_quality) &&  %w(A B F# C# G# D# A# E#).index(chord_root) \
@@ -60,65 +62,55 @@ class Chord
 	end
 
 	def determine_scale(chord_root, chord_quality)
-		if is_flat(chord_root, chord_quality)
+		if flat?(chord_root, chord_quality)
 			CHROMATIC_SCALE_FLAT
-		elsif is_sharp(chord_root, chord_quality)
+		elsif sharp?(chord_root, chord_quality)
 			CHROMATIC_SCALE_SHARP
 		end
 	end
 
 	def determine_interval(chromatic_scale, chord_root, chord_quality)
-		if chord_root == 'E#' && chord_quality == 'min'
+		if [chord_root, chord_quality] == ['E#', 'min']
 			chromatic_scale.index('F')
-		elsif chord_root == 'Bb' && chord_quality == 'aug'
+		elsif [chord_root, chord_quality] == ['Bb', 'aug']
 			chromatic_scale.index('A#')
 		else
 			chromatic_scale.index(chord_root)
-		end
+		end			
 	end
 
 	def chord_third(chord_quality)
-		case chord_quality
-		when 'maj', 'dom', 'aug'
-			MAJ3
-		when 'min', 'dim'
-			MIN3
-		end
+		{
+			'maj'=> MAJ3,
+			'dom'=> MAJ3,
+			'aug'=> MAJ3,
+			'min'=> MIN3,
+			'dim'=> MIN3
+		}[chord_quality]
 	end
 
 	def chord_fifth(chord_quality)
-		case chord_quality
-		when 'maj', 'dom', 'min'
-			PERF5
-		when 'aug'
-			AUG5
-		when 'dim'
-			DIM5
-		end
+		{
+			'maj'=> PERF5,
+			'dom'=> PERF5,
+			'min'=> PERF5,
+			'aug'=> AUG5,
+			'dim'=> DIM5
+		}[chord_quality]
 	end
 
 	def interval_number(interval_number, chord_quality)
-		if interval_number == 7
-			case chord_quality
-			when 'dim'
-				DIM7	
-			when 'dom', 'min', 'aug'
-				MIN7
-			when 'maj'
-				MAJ7
-			end	
-		elsif interval_number == 6
-			MAJ6
-		elsif interval_number == 'maj7'
-			MAJ7
-		end
-	end
-
-	def chord_notes_list1(chord_root, chord_third, chord_fifth)
-		"#{chord_root} #{chord_third} #{chord_fifth}"
-	end
-
-	def chord_notes_list2(chord_root, chord_third, chord_fifth, interval_added)
-		"#{chord_root} #{chord_third} #{chord_fifth} #{interval_added}"
+		{
+			'maj7' => MAJ7,
+			6 => MAJ6,
+			7 => 
+			{
+				'maj'=> MAJ7,
+				'dom'=> MIN7,
+				'min'=> MIN7,
+				'aug'=> MIN7,
+				'dim'=> DIM7
+			}[chord_quality]
+		}[interval_number]
 	end
 end
