72c1f38dbf397d616c22bf9a3ed9cc54b63f967b
siwka <ms.ksiwek@gmail.com>
2014-02-27 18:26:21 -0600

    sixth & seventh chord add

--- lib/chord.rb
+++ lib/chord.rb
@@ -10,27 +10,33 @@ class Chord
 		AUG5 = 8
 		# Added
 		MAJ6 = 9		# min6  to moze zapisac min6 & maj6 & M6 & m6 ma pozycje 9
-		DIM7 = 0	
+		DIM7 = 9
 		MIN7 = 10	
 		MAJ7 = 11	
 
 
-	def chord_interpretator(chord_root, chord_quality, interval_quality, alerted_fifth, added_tone_chords)
+	def chord_interpretator(chord_root, chord_quality, interval_number, alerted_fifth, added_tone_chords)
 		chromatic_scale = determine_scale(chord_root, chord_quality)
 		interval = determine_interval(chromatic_scale, chord_root, chord_quality)
 		
 		chord_third = chromatic_scale[transposition(chord_third(chord_quality), interval)]
 		chord_fifth = chromatic_scale[transposition(chord_fifth(chord_quality), interval)]
 
-		if interval_quality == 0
-			chord_root + ' '	+  chord_third + ' '	+  chord_fifth
-		elsif interval_quality == 6 || interval_quality ==7		
-			interval_added = chromatic_scale[transposition(interval_quality(interval_quality, chord_quality), interval)]
-			chord_root + ' '	+  chord_third + ' '	+  chord_fifth + ' ' + interval_added
+		if interval_number == 0
+			chord_notes_list1(chord_root, chord_third, chord_fifth)
 		else
-			puts 'wrong scanner output'
+			if interval_number == 7 || interval_number == 'maj7'
+				if chord_quality == 'aug' || chord_quality == 'min'
+					chromatic_scale = determine_scale(chord_root, 'dom')
+				elsif chord_quality == 'dim'
+					chromatic_scale = CHROMATIC_SCALE_FLAT
+				end
+			end	
+			# interval_number == 6, 7, 'maj7' :
+			interval_added = chromatic_scale[transposition(interval_number(interval_number, chord_quality), interval)]
+			chord_notes_list2(chord_root, chord_third, chord_fifth, interval_added)
+			#chord_root + ' '	+  chord_third + ' '	+  chord_fifth + ' ' + interval_added
 		end
- 
 	end
 
 
@@ -56,9 +62,9 @@ class Chord
 
 	def determine_scale(chord_root, chord_quality)
 		if is_flat(chord_root, chord_quality)
-			chromatic_scale = CHROMATIC_SCALE_FLAT
+			CHROMATIC_SCALE_FLAT
 		elsif is_sharp(chord_root, chord_quality)
-			chromatic_scale = CHROMATIC_SCALE_SHARP
+			CHROMATIC_SCALE_SHARP
 		end
 	end
 
@@ -75,35 +81,45 @@ class Chord
 	def chord_third(chord_quality)
 		case chord_quality
 		when 'maj', 'dom', 'aug'
-			chord_third = MAJ3
+			MAJ3
 		when 'min', 'dim'
-			chord_third = MIN3
+			MIN3
 		end
 	end
 
 	def chord_fifth(chord_quality)
 		case chord_quality
 		when 'maj', 'dom', 'min'
-			chord_fifth = PERF5
+			PERF5
 		when 'aug'
-			chord_fifth = AUG5
+			AUG5
 		when 'dim'
-			chord_fifth = DIM5
+			DIM5
 		end
 	end
 
-	def interval_quality(interval_quality, chord_quality)
-		if interval_quality == 7
+	def interval_number(interval_number, chord_quality)
+		if interval_number == 7
 			case chord_quality
 			when 'dim'
 				DIM7	
-			when 'dom'#ks add_this?, 'min', 'aug'
+			when 'dom', 'min', 'aug'
 				MIN7
 			when 'maj'
 				MAJ7
 			end	
-		elsif interval_quality == 6
+		elsif interval_number == 6
 			MAJ6
+		elsif interval_number == 'maj7'
+			MAJ7
 		end
-	end	
+	end
+
+	def chord_notes_list1(chord_root, chord_third, chord_fifth)
+		chord_root + ' '	+  chord_third + ' '	+  chord_fifth
+	end
+
+	def chord_notes_list2(chord_root, chord_third, chord_fifth, interval_added)
+		chord_root + ' '	+  chord_third + ' '	+  chord_fifth + ' ' + interval_added
+	end
 end
