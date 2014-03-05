e4565107f8486f3e88c9760373dc76f7d684fc6f
siwka <ms.ksiwek@gmail.com>
2014-03-04 20:32:15 -0600

    add first functionality: output text to scanner

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -6,6 +6,6 @@ class Scanner
 
 	def start
 		@messenger.puts "Welcome to note interpreter!"
-		@messenger.puts "Enter chord in jazz notation."
+		@messenger.puts "Enter chord in jazz notation.\n Note: for ♭ use b (lower case B),\n example: G#−7♭5 as G#−7b5."
 	end
 end
--- spec/scanner_spec.rb
+++ spec/scanner_spec.rb
@@ -2,18 +2,16 @@ require 'scanner'
 
 describe Scanner do
 	context "starting up" do
+		let(:messenger) { messenger = mock("messenger").as_null_object }
+		let(:scanner) { scanner = Scanner.new(messenger) }
 
 		it "should send a welcome message" do
-			messenger = mock("messenger").as_null_object
-			scanner = Scanner.new(messenger)
 			messenger.should_receive(:puts).with("Welcome to note interpreter!")
 			scanner.start
 		end
 
 		it "should prompt for the first chord" do
-			messenger = mock("messenger").as_null_object
-			scanner = Scanner.new(messenger)
-			messenger.should_receive(:puts).with("Enter chord in jazz notation.")
+			messenger.should_receive(:puts).with("Enter chord in jazz notation.\n Note: for ♭ use b (lower case B),\n example: G#−7♭5 as G#−7b5.")
 			scanner.start
 		end
 	end
